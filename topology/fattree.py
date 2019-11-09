5c557a6cfcbfa524950ab2b02712b05e45190720
Santiago39353535 <santiagomarinaro1@gmail.com>
2019-11-09 20:11:22 -0300

    topologia, falta hacer que el nivel se pase por parametro

--- topology/fattree.py
+++ topology/fattree.py
@@ -1,2 +1,41 @@
 from mininet.topo import Topo
 
+class FatTree(Topo):
+    my_switchs = []
+    my_hosts = []
+    def __init__( self, niveles = 2, **opts ):
+        Topo.__init__(self, **opts)
+
+        ###Crear switches
+        prefijoSW = "sw"
+        for lv in range(niveles):
+            nivel = []
+            for i in range(2**lv):
+                sw = self.addSwitch(prefijoSW+'_'+str(lv)+'_'+str(i))
+                nivel.append(sw)
+            self.my_switchs.append(nivel)
+            print("Llegue switch creacion nivel "+str(lv))
+
+        ###Creacion hosts
+        prefijoHost = "h"
+        for i in range((2**(niveles-1))+3):
+            host = self.addHost(prefijoHost+'_'+str(i))
+            self.my_hosts.append(host)
+            print("creacion host "+str(i))
+
+        ###Conexion de los hosts
+        for i in range(3):
+            host = self.my_hosts.pop(0)
+            self.addLink(self.my_switchs[0][0], host)
+        for switch in self.my_switchs[-1]:
+            host = self.my_hosts.pop(0)
+            self.addLink(switch, host)
+
+        ###conexxion switches
+        for lv in range(len(self.my_switchs)):
+            if lv < (niveles-1):
+                for switch1 in self.my_switchs[lv]:
+                    for switch2 in self.my_switchs[lv+1]:
+                        self.addLink(switch1, switch2)
+
+topos = { 'fattree': FatTree }
